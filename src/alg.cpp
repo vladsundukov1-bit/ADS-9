842234a08268836b37181bd8ff312cc069e6fe3d
vladsundukov1-bit <vladsundukov1@gmail.com>
2026-06-01 20:16:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,7 @@
 // Copyright 2022 NNTU-CS
-#include  "tree.h"
 #include <algorithm>
 #include <vector>
+#include  "tree.h"
 
 PMTree::PMTree(const std::vector<char>& src) : base(src) {
     top = new Node(0);
